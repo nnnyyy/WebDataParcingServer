d66c9d7197c69a5529c0437bd3b721e223651875
NEXON\nnnyyy <nnnyyy@nate.com>
2017-09-15 11:10:32 +0900

    네이버 관련 요청 리팩토링

--- WebDataParcingServer/routes/index.js
+++ WebDataParcingServer/routes/index.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var request = require('request');
-var cheerio = require('cheerio');
 var youtube = require('./youtube');
-var urlencode = require('urlencode');
 var mysql = require('../mysql/_Mysql');
+var naver = require('./naver');
 
 /* GET home page. */
 router.get('/', function(req, res_parent, next) {
@@ -19,34 +17,10 @@ router.get('/', function(req, res_parent, next) {
   })
 });
 
-router.get('/most10', function(req, res, next) {
-
-  var reqOptions = {
-    url: 'http://www.naver.com',
-    method: 'GET',
-    headers: {
-      'Accept' : 'application/xml',
-      'Accept-Charset' : 'utf-8',
-      'User-Agent' : 'my-reddit-client'
-    }
-  }
-
-  try {
-    request( reqOptions, function(err, res_inner, body) {
-
-      var aMost = [];
-      var $ = cheerio.load(body);
-      $('div.PM_CL_realtimeKeyword_rolling_base div.PM_CL_realtimeKeyword_rolling ul li span.ah_k').each(function() {
-        aMost.push({item:$(this).text()})
-      });
-
-      res.send({ data: aMost });
-    });
-  }
-  catch(err) {
-    console.log(err);
-    res.render('index', { title: 'Express - Error' });
-  }
+router.get('/most', function(req, res, next) {
+  naver.GetMost(function(s) {
+    res.send(s);
+  });
 });
 
 router.get('/dbtest', function(req, res, next){
@@ -54,25 +28,9 @@ router.get('/dbtest', function(req, res, next){
 })
 
 router.get('/news/:arg_query', function(req, res, next) {
-  query = urlencode(req.params.arg_query);
-  console.log(query);
-  var reqOptions = {
-    url: 'https://openapi.naver.com/v1/search/news?query='+ query +'&display=10&start=1&sort=sim',
-    method: 'GET',
-    headers: {
-      "X-Naver-Client-Id" : "7MP8gMwbVGDFjlm3wypW",
-      "X-Naver-Client-Secret" : "zkLeDVM3YZ",
-    }
-  }
-
-  try {
-    request( reqOptions, function(err, res_inner, body) {
-      obj = JSON.parse(body);
-      res.send({ ret: 'success', body: obj });
-    });
-  }catch(e){
-    res.send({ret: 'error'});
-  }
+  naver.GetNews(req.params.arg_query, function(s){
+    res.send(s);
+  });
 })
 
 module.exports = router;
---
+++ WebDataParcingServer/routes/naver.js
@@ -0,0 +1,58 @@
+/**
+ * Created by nnnyyy on 2017-09-15.
+ */
+var request = require('request');
+var cheerio = require('cheerio');
+var urlencode = require('urlencode');
+
+exports.GetMost = function(callback) {
+    var reqOptions = {
+        url: 'http://www.naver.com',
+        method: 'GET',
+        headers: {
+            'Accept' : 'application/xml',
+            'Accept-Charset' : 'utf-8',
+            'User-Agent' : 'my-reddit-client'
+        }
+    }
+
+    try {
+        request( reqOptions, function(err, res_inner, body) {
+            var aMost = [];
+            var $ = cheerio.load(body);
+            $('div.PM_CL_realtimeKeyword_rolling_base div.PM_CL_realtimeKeyword_rolling ul li span.ah_k').each(function() {
+                aMost.push({item:$(this).text()})
+            });
+
+            //res.send({ data: aMost });
+            callback({ret:0, data: aMost});
+        });
+    }
+    catch(err) {
+        console.log(err);
+        callback({ret:-1});
+        //res.render('index', { title: 'Express - Error' });
+    }
+}
+
+exports.GetNews = function(req_query, callback) {
+    query = urlencode(req_query);
+    var reqOptions = {
+        url: 'https://openapi.naver.com/v1/search/news?query='+ query +'&display=10&start=1&sort=sim',
+        method: 'GET',
+        headers: {
+            "X-Naver-Client-Id" : "7MP8gMwbVGDFjlm3wypW",
+            "X-Naver-Client-Secret" : "zkLeDVM3YZ",
+        }
+    }
+
+    try {
+        request( reqOptions, function(err, res_inner, body) {
+            obj = JSON.parse(body);
+            //res.send({ ret: 'success', body: obj });
+            callback({ret:0, news: obj});
+        });
+    }catch(e){
+        callback({ret:-1});
+    }
+}
