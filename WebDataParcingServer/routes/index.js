87fa2c5cd836d43de516598b6fc108ce54b50d95
NEXON\nnnyyy <nnnyyy@nate.com>
2017-09-15 10:30:39 +0900

    네이버 api 예제

--- WebDataParcingServer/routes/index.js
+++ WebDataParcingServer/routes/index.js
@@ -3,12 +3,13 @@ var router = express.Router();
 var request = require('request');
 var cheerio = require('cheerio');
 var youtube = require('./youtube');
+var urlencode = require('urlencode');
 var mysql = require('../mysql/_Mysql');
 
 /* GET home page. */
 router.get('/', function(req, res_parent, next) {
   var url_final = youtube.MakeVideoBaseURL(20);
-  youtube.YoutubeSearch(req.query, 'ê³ ì–‘ì´,ì•„ë¹„ì‹œë‹ˆì•ˆ,ì• êµ', '', function(ret) {
+  youtube.YoutubeSearch(req.query, '°í¾çÀÌ, ¾Æºñ½Ã´Ï¾È, ¾Ö±³', '', function(ret) {
     try {
       url_final += ('&id=' + JSON.parse(ret.sRet));
       youtube.YoutubeGetVideos(url_final, ret.nextToken, ret.prevToken, res_parent);
@@ -50,6 +51,28 @@ router.get('/most10', function(req, res, next) {
 
 router.get('/dbtest', function(req, res, next){
   var list = [];
+})
+
+router.get('/news/:arg_query', function(req, res, next) {
+  query = urlencode(req.params.arg_query);
+  console.log(query);
+  var reqOptions = {
+    url: 'https://openapi.naver.com/v1/search/news?query='+ query +'&display=10&start=1&sort=sim',
+    method: 'GET',
+    headers: {
+      "X-Naver-Client-Id" : "7MP8gMwbVGDFjlm3wypW",
+      "X-Naver-Client-Secret" : "zkLeDVM3YZ",
+    }
+  }
+
+  try {
+    request( reqOptions, function(err, res_inner, body) {
+      obj = JSON.parse(body);
+      res.send({ ret: 'success', body: obj });
+    });
+  }catch(e){
+    res.send({ret: 'error'});
+  }
 })
 
 module.exports = router;
